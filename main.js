ca8a48f45e115caf79d79d5643a32237ded5087f
MwM-Mai <1030197150@qq.com>
2022-07-16 16:27:20 +0800

    完成 home 首页的 开发

--- main.js
+++ main.js
@@ -3,6 +3,40 @@
 import Vue from 'vue'
 import App from './App'
 
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+
+// 挂载到顶级的对象之上 uniapp 顶级对象是 uni
+uni.$http = $http
+
+// 请求的根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+
+// 如果在每次请求拦截之前显示 loading 效果 请求之后隐藏 loading 效果 就要使用 拦截器
+// 请求拦截器 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  // 例如显示loading
+  uni.showLoading({
+      title: '数据加载中...',
+    })
+}
+
+// 响应拦截器 请求完成之后做一些事情
+$http.afterRequest = function () {
+  // 隐藏loading效果
+  uni.hideLoading()
+}
+
+// 封装显示消息的提示方法
+uni.$showMsg = function (title = '数据加载失败!', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })
+}
+
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,45 @@
 <template>
   <view>
-    home
+    <!-- 轮播图的区域 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="(item, index) in swiperList" :key="index">
+        <navigator :url="`/subpkg/goos_detail/goos_detail?goos_id=${item.goods_id}`" class="swiper-item">
+          <image :src="item.image_src"></image>
+        </navigator>
+      </swiper-item>
+    </swiper>
+    
+    <!-- 分类导航区域 -->
+    <view class="nav-lsit">
+      <view v-for="(item, index) in navList" :key="index" class="nav-item" @click="navClickHandler(item)"> 
+        <img :src="item.image_src" class="nav-img">
+      </view>
+    </view>
+    
+    <!-- 楼层区域 -->
+    <!-- 楼层容器 -->
+    <view class="floor-list">
+      <!-- 每一个楼层的item项 -->
+      <view class="floor-item" v-for="(item, index) in floorList" :key="index">
+        <!-- 楼层的标题 -->
+        <image :src="item.floor_title.image_src" class="floor-title"></image>
+        <!-- 楼层的图片区域 -->
+        <view class="floor-img-box">
+          <!-- 左侧大图片 mode="widthFix" 高度自适应-->
+          <navigator class="letf-img-box" :url="item.product_list[0].url">
+            <image :src="item.product_list[0].image_src" :style="{width: item.product_list[0].image_width + 'rpx'}" mode="widthFix"></image>
+          </navigator>
+          <!-- 右侧四个小图片 -->
+          <view  class="right-img-box">
+            <view  v-for="(item2, index2) in item.product_list" :key="index2">
+              <navigator v-if="index2 !== 0" :url="item2.url">
+                <image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}"></image>
+              </navigator>
+            </view>
+          </view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -8,12 +47,109 @@
   export default {
     data() {
       return {
-        
+        // 1. 轮播图的数据列表
+        swiperList: [],
+        // 2. 分类导航数据列表
+        navList: [],
+        // 3. 楼层数据列表
+        floorList: [],
       };
+    },
+    // 生命周期函数 挂载
+    onLoad() {
+      // 调用方法获取轮播图数据
+      this.getSwiperList() 
+      // 调用方法获取分类导航数据
+      this.getNavList()
+      // 调用方法获取楼层数据
+      this.getFloorList()
+    },
+    methods: {
+      // 获取轮播图数据
+      async getSwiperList() {
+        const {data: res} = await uni.$http.get('/api/public/v1/home/swiperdata')
+        // 请求失败
+        if(res.meta.status !== 200) return uni.$showMsg()
+        
+        // 请求成功
+         this.swiperList = res.message
+         uni.$showMsg(res.meta.msg)
+      },
+      
+      // 获取分类导航数据
+      async getNavList() {
+        const {data: res} = await uni.$http.get('/api/public/v1/home/catitems')
+        // 请求失败
+        if(res.meta.status !== 200) return uni.$showMsg()
+        // 请求成功 
+        this.navList = res.message
+        uni.$showMsg(res.meta.msg)
+      },
+      
+      //  获取楼层数据
+      async getFloorList() {
+        const {data: res} = await uni.$http.get('/api/public/v1/home/floordata')
+        // 请求失败
+        if(res.meta.status !== 200) return uni.$showMsg()
+        // 请求成功 
+        // 对数据进行处理 切换页面的路径不对
+        res.message.forEach(floor => {
+          floor.product_list.forEach(prod => {
+            prod.url = '/subpkg/goos_list/goos_list?' + prod.navigator_url.split('?')[1]
+          })
+        })
+        this.floorList = res.message
+      },
+      
+      
+      
+      
+      
+      // 点击分类导航页面跳转
+      navClickHandler(item) {
+        if(item.name == '分类') {
+          uni.switchTab({
+            url: "/pages/cate/cate"
+          })
+        }
+      }
     }
   }
 </script>
 
 <style lang="scss">
+swiper{ 
+  height: 330rpx;
+  .swiper-item,
+   image {
+     width: 100%;
+     height: 100%;
+   }
+}
 
+.nav-lsit {
+  display: flex;
+  justify-content: space-around;
+  margin: 15px 0;
+  .nav-img {
+    width: 128rpx;
+    height: 140rpx;
+  }
+}
+
+.floor-title {
+  width: 100%;
+  height: 60rpx;
+}
+
+.right-img-box {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.floor-img-box {
+  display: flex;
+  padding-left: 10rpx;
+}
 </style>
